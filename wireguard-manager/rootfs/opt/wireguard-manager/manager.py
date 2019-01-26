bb48b88afffc31b612176b0d2b9ca4ec508b6be3
whiskerz007 <2713522+whiskerz007@users.noreply.github.com>
2019-01-27 00:35:56 +0100

    Added database

--- wireguard-manager/rootfs/opt/wireguard-manager/manager.py
+++ wireguard-manager/rootfs/opt/wireguard-manager/manager.py
@@ -1,8 +1,37 @@
 from flask import Flask, render_template, url_for, flash, redirect
+from flask_sqlalchemy import SQLAlchemy
 from forms import InterfaceForm, PeerForm
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = '6a598f99f0915fd9f158ae5597aad87d'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:////data/wg.db'
+db = SQLAlchemy(app)
+
+
+class Interface(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    interface = db.Column(db.Integer, unique=True, nullable=False)
+    port = db.Column(db.Integer, unique=True, nullable=False)
+    address = db.Column(db.String(), unique=True, nullable=False)
+    subnet_mask = db.Column(db.Integer, nullable=False)
+    private_key = db.Column(db.String(44), unique=True, nullable=False)
+    peers = db.relationship('Peer', backref='interface', lazy=True)
+
+    def __repr__(self):
+        return f"Interface('wg{self.interface}', '{self.address}/{self.subnet_mask}')"
+
+class Peer(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    peer = db.Column(db.String(20), unique=True, nullable=False)
+    public_key = db.Column(db.String(44), nullable=False)
+    endpoint = db.Column(db.String())
+    allowed_ips = db.Column(db.String())
+    persistent = db.Column(db.Integer)
+    interface_id = db.Column(db.Integer, db.ForiegnKey('integer.id'), nullable=False)
+
+    def __repr__(self):
+        return f"Peer('{self.peer}', '{self.endpoint}/{self.allowed_ips}')"
+
 
 @app.route('/')
 def home():
