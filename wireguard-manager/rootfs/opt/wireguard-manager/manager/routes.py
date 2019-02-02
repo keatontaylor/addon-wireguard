64b6641dc27a120e6ef55ab1e1e74ff2b4e93029
whiskerz007 <2713522+whiskerz007@users.noreply.github.com>
2019-02-02 03:35:12 +0100

    Test private_key if valid

--- wireguard-manager/rootfs/opt/wireguard-manager/manager/routes.py
+++ wireguard-manager/rootfs/opt/wireguard-manager/manager/routes.py
@@ -2,6 +2,7 @@ from flask import render_template, url_for, flash, redirect
 from manager import app
 from manager.forms import InterfaceForm, PeerForm
 from manager.models import Interface, Peer
+from subprocess import run, PIPE
 
 @app.route('/')
 def home():
@@ -13,6 +14,18 @@ def interface():
     if form.validate_on_submit():
         if form.private_key.data:
             # Run `wg pubkey` against private_key entered
+            # p = run(['wg', 'genkey'], stdout=PIPE)
+            # print(p.stdout.decode().rstrip())
+
+            p = run(["wg", "pubkey"],
+                    stdout=PIPE,
+                    stderr=PIPE,
+                    input=str.encode(form.private_key.data))
+            # print(p)
+            # CompletedProcess(args=['wg', 'pubkey'], returncode=0, stdout=b'')
+            if p.returncode != 0:
+                flash(p.stderr.decode().split(': ')[1].rstrip())
+                return render_template('interface.html', title='', form=form)
         
         flash(f'You have submitted a valid form!', 'success')
         return redirect(url_for('home'))
